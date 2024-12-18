02ffb0b663b2367d0596379b6f9feb740a4ab07e
Paulmp <pplathjr@hotmail.com>
2024-12-18 23:00:08 +0100

    Placed graph in panel. Added footer.

--- lonercordplot/src/routes/+page.svelte
+++ lonercordplot/src/routes/+page.svelte
@@ -21,8 +21,8 @@
 	});
 
 	function plotData(dataFile: any) {
-		const width = 1050;
-		const height = 750;
+		const width = 850;
+		const height = 650;
 		const margin = { top: 20, right: 30, bottom: 50, left: 50 };
 		const dataLength = dataFile.length;
 		const xScale = d3
@@ -52,6 +52,7 @@
 					.attr('text-anchor', 'end')
 					.text('Secondary Psychopathy')
 					.attr('color', 'white')
+					.attr('font-size', '20px')
 			);
 
 		// Add y-axis to the SVG
@@ -67,6 +68,7 @@
 					.attr('fill', 'white')
 					.attr('text-anchor', 'start')
 					.text('Primary Psychopathy')
+					.attr('font-size', '20px')
 			);
 
 		// Add circles for the scatter plot
@@ -98,20 +100,23 @@
 	}
 </script>
 
-<div class="flex min-h-screen flex-col bg-inherit">
-	<header class="flex items-center space-x-3 bg-purple-900/[0.6] p-2 text-white">
-		<img src="/images/LonerBox.png" alt="Lonerbox" class="w-20" />
-		<h1 class="font-mono text-3xl font-bold">Lonercord's Psychopathy Scores</h1>
-	</header>
+<header class="flex items-center space-x-3 bg-purple-900/[0.6] p-2 text-white">
+	<img src="/images/LonerBox.png" alt="Lonerbox" class="w-20" />
+	<h1 class="font-mono text-3xl font-bold">Lonercord's Psychopathy Scores</h1>
+</header>
 
-	<section class="flex flex-grow flex-col items-center p-8">
-		<div>
-			<h2 class="animate-slideUpAndFade mb-4 text-2xl font-bold text-white">Psychopathy Plot</h2>
-		</div>
-		<div class="animate-slideUpAndFade text-white" id="plot"></div>
-	</section>
-
-	<!-- <footer class="bg-gray-800 p-4 text-center text-white">
+<div class="animate-slideUpAndFade flex flex-col w-full bg-inherit min-h-screen">
+	<div class="flex flex-1 items-center justify-center gap-">
+		<section
+			class="flex w-3/6 flex-col items-center justify-center rounded-xl border-4 border-black bg-zinc-800 p-0"
+		>
+			<div>
+				<h2 class="mt-4 text-2xl font-bold text-white">Psychopathy Plot</h2>
+			</div>
+			<div class="text-white" id="plot"></div>
+		</section>
+	</div>
+	<footer class="flex items-end justify-center bg-gray-800 p-4 text-center text-white">
 		<p>&copy; 2024 Lonercord Users. All rights reserved.</p>
-	</footer> -->
+	</footer>
 </div>
