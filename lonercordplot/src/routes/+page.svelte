4c30626993254e04a28fab04aa288f74eceabe22
Paulmp <pplathjr@hotmail.com>
2024-12-18 01:02:49 +0100

    Layout changes. Logo added. Data added.

--- lonercordplot/src/routes/+page.svelte
+++ lonercordplot/src/routes/+page.svelte
@@ -1,24 +1,23 @@
 <script lang="ts">
+	import type { PageData } from './$types';
+	import { select, scaleLinear, axisBottom, axisLeft } from 'd3';
+
+	let { data }: { data: PageData } = $props();
 </script>
 
-<main class="flex min-h-screen flex-col bg-gray-100">
-	<header class="bg-blue-600 p-4 text-white">
-		<h1 class="text-3xl">SvelteKit with Tailwind CSS</h1>
+<main class="flex min-h-screen flex-col bg-inherit">
+	<header class="flex items-center bg-purple-900/[0.6] p-3 text-white space-x-3 p-2">
+		<img src="/images/LonerBox.png" alt="Lonerbox" class="w-20" />
+		<h1 class="font-mono text-3xl font-bold">Lonercord's Psychopathy Scores</h1>
 	</header>
 
 	<section class="flex flex-grow flex-col items-center justify-center p-8">
-		<div class="text-center">
-			<h2 class="mb-4 text-2xl">Welcome to SvelteKit with Tailwind</h2>
-			<p class="text-gray-700">
-				This is a simple example of a page using SvelteKit and Tailwind CSS.
-			</p>
+		<div>
+			<h2 class="mb-4 text-2xl text-white">Test</h2>
 		</div>
 	</section>
 
-	<footer class="bg-gray-800 p-4 text-center text-white">
-		<p>&copy; 2024 Your Name. All rights reserved.</p>
-	</footer>
+	<!-- <footer class="bg-gray-800 p-4 text-center text-white">
+		<p>&copy; 2024 Lonercord Users. All rights reserved.</p>
+	</footer> -->
 </main>
-
-<style>
-</style>
